ea30b43c18d53aa241a04bd5fc88c29398d7d353
Meeka-Reese <157521411+Meeka-Reese@users.noreply.github.com>
2026-05-14 21:43:25 -0700

    Little Debug

--- webgl-demo.js
+++ webgl-demo.js
@@ -1639,6 +1639,7 @@ async function FrameCount()
 }
 const MainLoop = ()=>
 {
+  console.log(gAudioContext.state);
     gTime = new Date();
     let newTime = gTime.getTime();
     gDeltaTime = newTime - gPreviousTime;
